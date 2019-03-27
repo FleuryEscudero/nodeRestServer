2df6e0aacde4086621ae6b8ab675a2e226d1a0a1
FleuryEscudero <fleury.ea.27@gmail.com>
2019-03-27 01:00:13 -0600

    Categorias

---
+++ server/routes/category.routes.js
@@ -0,0 +1,112 @@
+
+
+var express = require('express');
+var _ = require('underscore');
+const Category = require ('../models/category.models');
+var { ensureAuth } = require ('../middlewares/authentication');
+var app = express ();
+
+app.get('/categories',ensureAuth, (req,res)=>{
+    Category.find({status:true}).exec((err,categories)=>{
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            categories
+        })
+    })
+})
+
+
+app.get('/category/:id',ensureAuth, (req,res)=>{
+    let id = req.params.id;
+    Category.findById(id,{ status:true})
+    .exec((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            category
+        })
+    })
+})
+
+app.post('/addCategory',ensureAuth, (req,res)=>{
+    let body = req.body;
+    let category = new Category({
+        description: body.description,
+        user: req.user._id
+    });
+        category.save((err, categoryDb)=>{
+            if(err){
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                caterory:categoryDb
+            })
+        })
+})
+
+
+app.put('/updateCategory/:id',ensureAuth, (req,res)=>{
+    let id = req.params.id;
+    let body = _.pick(req.body,['description']);
+
+    Category.findByIdAndUpdate(id, body, {new: true},(err, categoryDb)=>{
+        if(err){
+            res.status(400).json({
+               ok: false,
+               err
+           });
+           
+       }         
+       res.json({
+           ok: true,
+          user: categoryDb
+       });
+    })
+})
+
+
+app.delete('/deleteCategory/:id',(req,res)=>{
+    let id = req.params.id;
+    let nStatus = { status:false};
+
+    Category.findByIdAndUpdate(id, nStatus, {new:true}, (err,categoryDeleted)=>{
+        if(err){
+            res.status(400).json({
+               ok: false,
+               err
+           }); 
+        }
+
+        if (!categoryDeleted){
+            res.status(404).json({
+                ok: false,
+                err: {message:'La Categoria no ha podido ser borrado ya que no existe'}
+            })
+        }
+        res.json({
+            ok: true,
+            vategory: categoryDeleted
+        });
+    })
+})
+
+
+
+
+
+module.exports = app;
